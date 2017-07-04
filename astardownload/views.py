5c66218e284d0d516ac9546efe45b13bcd42f6d8
unknown <>
2017-07-04 16:26:11 +0800

    创建AStar download项目

---
+++ astardownload/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+# Create your views here.
+class UserForm(forms.Form):
+    name = forms.CharField()
+def register(req):
+    if req.method == 'POST':
+
+        form = UserForm(req.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponse('ok')
+    else :
+        form = UserForm()
+    return render_to_response('register.html',{'form':form})
