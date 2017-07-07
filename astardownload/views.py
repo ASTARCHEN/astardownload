65bea1ae3491e3078e2bd3cacc0de73d2f495f9d
zzxzwj <2377389590@qq.com>
2017-07-07 19:03:25 +0800

    增加下载功能

--- astardownload/views.py
+++ astardownload/views.py
@@ -54,7 +54,24 @@ def do_download(req):
     pass
 
 def do_admin(req):
-    pass
+    if req.method == 'POST':
+        uf = UserFormUpload(req.POST, req.FILES)
+        if uf.is_valid():
+            # 得到uploadedfile类型的文件对象
+            myfile = uf.cleaned_data['myfile']
+            prefix_upload = cp.get('dirs','prefix_upload')
+            # 得到文件名
+            myfile_name = myfile.name
+            # fp = file(prefix_upload + myfile_name, 'wb')
+            fp = open(prefix_upload+myfile_name,'wb')
+            fp.write(myfile.read())
+            fp.close()
+            return HttpResponse('ok')
+    else:
+        uf = UserFormUpload()
+    mystr = cp.get('html','upload')
+    print(mystr)
+    return render_to_response(mystr, {'uf': uf})
 
 def do_index(req):
     return render_to_response(cp.get('html','index'))
